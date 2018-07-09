f71da3ad7abd82940116528f02ad05bcfee1034f
Zelalem Tenaw Terefe <zterefe@fb.com>
2018-07-09 15:24:55 -0700

    Initial Commit

---
+++ instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instagram
+//
+//  Created by Zelalem Tenaw Terefe on 7/9/18.
+//  Copyright © 2018 Zelalem Tenaw Terefe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instagram
+//
+//  Created by Zelalem Tenaw Terefe on 7/9/18.
+//  Copyright © 2018 Zelalem Tenaw Terefe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
